fae2132b3d0140534eb2432a801b590956f2c66b
kryptwire <88946043+kryptwire@users.noreply.github.com>
2026-07-17 16:44:59 -0400

    it works now

--- readjoystickvalues.ino
+++ readjoystickvalues.ino
@@ -1,5 +1,6 @@
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(A1, INPUT);
   pinMode(A0, INPUT);
 }
@@ -9,8 +10,10 @@ void loop() {
   int valueX = analogRead(A1);
   int valueY = analogRead(A0);
 
-  Serial.print("X-value: " + valueX);
-  Serial.print("Y-value: " + valueY);
+  Serial.print("X-value: ");
+  Serial.println(valueX);
+  Serial.print("Y-value: ");
+  Serial.println(valueY);
 
   delay(1000);
 }
